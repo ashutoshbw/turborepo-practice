dc29d9cc41b77be33681052fc08edd0af5ea76f1
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-11-07 23:23:43 +0600

    initial commit

---
+++ apps/website/src/app/docs/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import { readFile, readdir } from "node:fs/promises";
+import path from "node:path";
+import matter from "gray-matter";
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+const contentSource = "docs";
+
+export async function generateStaticParams() {
+  const filenames = await readdir(path.join(process.cwd(), contentSource));
+  return filenames.map((name) => ({ slug: name.replace(".mdx", "") }));
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+  const filePath = path.join(process.cwd(), contentSource, slug + ".mdx");
+  const file = await readFile(filePath, "utf8");
+
+  const { data, content } = matter(file);
+
+  return (
+    <article>
+      <h1>{data.title}</h1>
+      <MDXRemote source={content} />
+    </article>
+  );
+}
