70f147f0bf4ee4b3d412ecb0da70f45706eec518
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-11-10 01:05:13 +0600

    mosquito

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
+          <li>A new list with mosquito</li>
           <li>A new list with tea</li>
           <li>
             Coffee.... Start with something different. Save and see your changes
