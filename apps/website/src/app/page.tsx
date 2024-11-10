ecf3d97f43df51f50ce4833766eff00d3e71314f
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-11-10 16:36:04 +0600

    add a new item

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -20,6 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
+          <li>Laptop</li>
           <li>water</li>
           <li>m wi</li>
           <li>A new list with more mosquito</li>
