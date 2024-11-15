d7e1ce8b5c7c5d2a875abfad8c474d7eb2bc3f74
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-11-15 22:31:36 +0600

    add tslint

--- packages/math/eslint.config.js
+++ packages/math/eslint.config.js
@@ -1,12 +1,12 @@
+// @ts-check
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default tseslint.config(
+  { ignores: ["dist/*"] },
+  { languageOptions: { globals: globals.browser } },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic
+);
