e4294aefa5b59e60a990de8d9a8de1077d83d0fd
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-11-17 02:46:07 +0600

    update page added extra extra

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Result of add: {add(3, 10)}..!!! Extra</li>
+          <li>Result of add: {add(3, 10)}..!!! Extra. Extra</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
