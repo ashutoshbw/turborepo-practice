319a13c449dcb58bc64f78f431c90d3994379c63
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-11-17 20:33:35 +0600

    again...

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   trailingSlash: true,
-  output: 'export'
+  output: "export",
+  distDir: "dist",
 };
 
 export default nextConfig;
