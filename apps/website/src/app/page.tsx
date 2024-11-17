000f407c919c4954faa896eaa5558b33a7835d64
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-11-17 18:11:57 +0600

    a new change

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
             .
           </li>
           <li>Result of add: {add(3, 10)}..!!! Extra. Extra. Final Extra</li>
-          <li>Turbo.json changed</li>
+          <li>another change</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
