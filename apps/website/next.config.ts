da85b105ea8a368b8291bcad3c5358caeec85c89
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-11-19 17:02:35 +0600

    use nexts preffered way to use mdx

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  trailingSlash: true,
-  output: "export",
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
---
+++ apps/website/src/app/mdx-page/my-component.tsx
@@ -0,0 +1,7 @@
+export function MyComponent() {
+  return (
+    <div className="text-4xl m-4 bg-teal-400 text-blue-950 font-bold">
+      Hello MDX!
+    </div>
+  );
+}
